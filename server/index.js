5850779f53d620222541ef8b6b4ba9082c8a147f
gourab Das <bisal.das2000@gmail.com>
2024-06-17 23:27:32 +0530

    sending message

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/test', (req, res) => {
-    res.json('Hello from backend');
+app.get('/', (req, res) => {
+    res.send('Hello from backend');
 })
 
 app.listen(4000, () => {
