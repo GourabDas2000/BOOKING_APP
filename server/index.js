7094af61d4607eafc414a9b84e207d1445409644
gourab Das <bisal.das2000@gmail.com>
2024-06-17 23:24:10 +0530

    commiting again

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/test', (req, res) => {
-    res.json('test ok');
+    res.json('Hello from backend');
 })
 
 app.listen(4000, () => {
