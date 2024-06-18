221fc961977fb00c90c6c2a09470250fae0ef28e
gourab Das <bisal.das2000@gmail.com>
2024-06-18 18:19:57 +0530

    after successfully connecting server

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: 'http://localhost:5173'
+    origin: ['http://localhost:5173', 'https://booking-app-three-fawn.vercel.app/']
 }));
 
 app.get('/', (req, res) => {
