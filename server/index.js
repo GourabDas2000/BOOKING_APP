260e28c0b46624b9125d3eb2da698eee42eadca6
gourab Das <bisal.das2000@gmail.com>
2024-06-18 17:48:25 +0530

    again trying

--- server/index.js
+++ server/index.js
@@ -3,7 +3,10 @@ const cors = require('cors');
 const app = express();
 
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
+
 app.get('/', (req, res) => {
     res.send('Hello from backend');
 });
