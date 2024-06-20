06bc07de4f5cf37b4d42bf4c349823af3e86c882
gourab Das <bisal.das2000@gmail.com>
2024-06-20 18:45:47 +0530

    successfully token generated

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('./src/models/User')
 require('dotenv').config();
 
@@ -13,7 +14,8 @@ app.use(express.json());
 app.use(cors({
     origin: ['http://localhost:5173',
         'https://booking-app-three-fawn.vercel.app'
-    ]
+    ],
+    credentials: true
 }));
 
 
@@ -28,6 +30,33 @@ app.post('/register', async(req, res) => {
         res.send(userDoc);
     } catch (e) {
         res.status(422).send('Something went wrong , try again later')
+    }
+});
+
+app.post('/login', async(req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (user) {
+            const passMatch = bcrypt.compareSync(password, user.password);
+            if (passMatch) {
+                jwt.sign({
+                    username: user.name,
+                    user_id: user._id
+                }, process.env.TOKEN, (err, token) => {
+                    if (err) {
+                        throw err;
+                    }
+                    res.cookie('Token', token, { httpOnly: true, secure: true, sameSite: 'strict' }).json('verified');
+                })
+            } else {
+                res.status(401).send('Username or password are incorrect');
+            }
+        } else {
+            res.status(404).send('User not found');
+        }
+    } catch (error) {
+        res.status(500).send('Internal server error');
     }
 });
 
